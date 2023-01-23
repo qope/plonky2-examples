d36d671e387e790fc42947cdd03259c50402f370
nuno <41233686+qope@users.noreply.github.com>
2023-01-23 23:54:41 +0900

    fix recursive proof

--- examples/simple_recursion.rs
+++ examples/simple_recursion.rs
@@ -1,18 +1,18 @@
 use anyhow::Result;
 use plonky2::{
-    field::{extension::Extendable, goldilocks_field::GoldilocksField, types::Field},
+    field::{extension::Extendable, goldilocks_field::GoldilocksField},
     hash::hash_types::RichField,
     iop::witness::{PartialWitness, WitnessWrite},
     plonk::{
         circuit_builder::CircuitBuilder,
-        circuit_data::{CircuitConfig, CommonCircuitData, VerifierCircuitTarget},
+        circuit_data::{CircuitConfig, VerifierCircuitData, VerifierCircuitTarget},
         config::{GenericConfig, PoseidonGoldilocksConfig},
         proof::ProofWithPublicInputs,
     },
 };
 
 fn make_first_proof<F, C, const D: usize>(
-) -> Result<(CommonCircuitData<F, D>, ProofWithPublicInputs<F, C, D>)>
+) -> Result<(VerifierCircuitData<F, C, D>, ProofWithPublicInputs<F, C, D>)>
 where
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
@@ -32,37 +32,42 @@ where
     pw.set_target(x_t, F::from_canonical_u64(2)); // x = 2
     let proof = data.prove(pw)?;
     data.verify(proof.clone())?;
-    Ok((data.common, proof))
+    Ok((data.verifier_data(), proof))
 }
 
 fn main() -> Result<()> {
     const D: usize = 2;
     type F = GoldilocksField;
     type C = PoseidonGoldilocksConfig;
+
+    let (verifier_data, proof) = make_first_proof::<F, C, D>()?;
     println!("First proof passed!");
 
     // Recursive proof
-    let mut builder_recursive = CircuitBuilder::<F, 2>::new(config);
-    let mut pw_recursive = PartialWitness::<F>::new();
-    let proof_t = builder_recursive.add_virtual_proof_with_pis(&data.common);
-    builder_recursive.register_public_inputs(&proof_t.public_inputs);
-    pw_recursive.set_proof_with_pis_target(&proof_t, &proof);
-    let vd_target = VerifierCircuitTarget {
-        constants_sigmas_cap: builder_recursive
-            .add_virtual_cap(data.common.config.fri_config.cap_height),
-    };
-    pw_recursive.set_cap_target(
-        &vd_target.constants_sigmas_cap,
-        &data.verifier_only.constants_sigmas_cap,
-    );
-    builder_recursive.verify_proof(proof_t, &vd_target, &data.common);
-    let data_recursive = builder_recursive.build::<C>();
-    let proof_recursive = data_recursive.prove(pw_recursive).unwrap();
-    match data_recursive.verify(proof_recursive.clone()) {
-        Ok(()) => println!("Recursive proof: Ok!"),
-        Err(x) => println!("{}", x),
-    }
-    println!("public inputs :{:?}", proof_recursive.public_inputs);
+    let config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(config);
 
+    let proof_t = builder.add_virtual_proof_with_pis::<C>(&verifier_data.common);
+    builder.register_public_inputs(&proof_t.public_inputs); // register first proof's public input
+
+    let constants_sigmas_cap_t =
+        builder.constant_merkle_cap(&verifier_data.verifier_only.constants_sigmas_cap);
+
+    let circuit_digest_t = builder.constant_hash(verifier_data.verifier_only.circuit_digest);
+    let verifier_circuit_t = VerifierCircuitTarget {
+        constants_sigmas_cap: constants_sigmas_cap_t,
+        circuit_digest: circuit_digest_t,
+    };
+
+    builder.verify_proof::<C>(&proof_t, &verifier_circuit_t, &verifier_data.common);
+
+    let mut pw = PartialWitness::<F>::new();
+    pw.set_proof_with_pis_target(&proof_t, &proof);
+    let data = builder.build::<C>();
+    let proof_recursive = data.prove(pw)?;
+    data.verify(proof_recursive.clone())?;
+
+    println!("Recursive proof passed!");
+    println!("public inputs :{:?}", proof_recursive.public_inputs);
     Ok(())
 }
